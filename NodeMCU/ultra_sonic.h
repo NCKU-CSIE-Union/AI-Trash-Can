4f5ea07e4a2c2b98f8f1391edee72d17102b57c4
jason810496 <zhu424.dev@gmail.com>
2025-01-15 22:25:39 +0800

    Fix ultra_sonic

--- NodeMCU/ultra_sonic.h
+++ NodeMCU/ultra_sonic.h
@@ -2,33 +2,35 @@
 #define ULTRA_SONIC_H
 
 #include <Arduino.h>
+#include "env.h"
 
 
 //define sound velocity in cm/uS
 #define SOUND_VELOCITY 0.034
 
-const int trigPin = 14;
-const int echoPin = 12;
+
 double getUltraSonicDistance(){
-    // Clears the trigPin
-    digitalWrite(trigPin, LOW);
+    // Clears the atoi(TRIG_PIN)
+    digitalWrite(atoi(TRIG_PIN), LOW);
     delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
+    // Sets the atoi(TRIG_PIN) on HIGH state for 10 micro seconds
+    digitalWrite(atoi(TRIG_PIN), HIGH);
     delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
+    digitalWrite(atoi(TRIG_PIN), LOW);
     
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
+    // Reads the atoi(ECHO_PIN), returns the sound wave travel time in microseconds
+    int duration = pulseIn(atoi(ECHO_PIN), HIGH);
     
     // Calculate the distance
-    distanceCm = duration * SOUND_VELOCITY/2;
+    int distanceCm = duration * SOUND_VELOCITY/2;
     
     // Convert to inches
     
     // Prints the distance on the Serial Monitor
     Serial.print("Distance (cm): ");
     Serial.println(distanceCm);
+
+    return distanceCm;
 }
 
 
